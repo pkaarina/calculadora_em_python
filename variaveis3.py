5e0620e0d4ecd01eed3fafe83f91d9b36dcfa0a7
Karina Ferreira <karinaraquel218@gmail.com>
2025-03-17 20:39:31 -0300

    atualizando calculadora

--- variaveis3.py
+++ variaveis3.py
@@ -1,40 +1,57 @@
 import sys
+from colorama import Fore, Style, init
 
-print('\n========= Bem vindo a calculadora em Python =========\n')
+init(autoreset=True)
 
-numero_um = input('digite o numero um: ')
+def print_banner():
+    banner = f"""
+{Fore.LIGHTBLUE_EX}
+#   ██████╗ █████╗ ██╗      ██████╗██╗   ██╗██╗      █████╗ ██████╗  ██████╗ ██████╗  █████╗  
+#  ██╔════╝██╔══██╗██║     ██╔════╝██║   ██║██║     ██╔══██╗██╔══██╗██╔═══██╗██╔══██╗██╔══██╗ 
+#  ██║     ███████║██║     ██║     ██║   ██║██║     ███████║██║  ██║██║   ██║██████╔╝███████║ 
+#  ██║     ██╔══██║██║     ██║     ██║   ██║██║     ██╔══██║██║  ██║██║   ██║██╔══██╗██╔══██║ 
+#  ╚██████╗██║  ██║███████╗╚██████╗╚██████╔╝███████╗██║  ██║██████╔╝╚██████╔╝██║  ██║██║  ██║ 
+#   ╚═════╝╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝ 
+{Style.RESET_ALL}
+{Fore.LIGHTBLUE_EX}Calculadora em Python - by Karina Ferreira {Style.RESET_ALL}
+"""
+    print(banner)
+
+print_banner()
+
+numero_um = input(f'{Fore.LIGHTMAGENTA_EX}digite o numero um: ')
 try:
     numero_um_convertido = float(numero_um)
 except ValueError:
-    print("Erro: a string não pode ser convertida em número")
+    print(f"{Fore.LIGHTRED_EX}Erro: a string não pode ser convertida em número")
     sys.exit()
 
-numero_dois = input('digite o numero dois: ')
+numero_dois = input(f'{Fore.LIGHTMAGENTA_EX}digite o numero dois: ')
 try:
     numero_dois_convertido = float(numero_dois)
 except ValueError:
-    print("Erro: a string não pode ser convertida em número")
+    print(f"{Fore.LIGHTRED_EX}Erro: a string não pode ser convertida em número")
     sys.exit()
 
-operacao=input("digite:\n\t+ para somar\n\t* para multiplicar\n\t- para subtrair\n\te / para dividir\n")
+operacao=input(f"{Fore.LIGHTMAGENTA_EX}digite:\n\t+ para somar\n\t* para multiplicar\n\t- para subtrair\n\te / para dividir\n")
 
 
 if operacao == "+":
-    print ("a operação escolhida foi: soma")
-    print ("o resultado da soma é: ",numero_um_convertido+numero_dois_convertido)
+    print (f"{Fore.LIGHTRED_EX}a operação escolhida foi: soma")
+    print (f"{Fore.LIGHTRED_EX}o resultado da soma é: {numero_um_convertido+numero_dois_convertido}")
 elif operacao == "*":
-    print ("a operação escolhida foi: multiplição")
-    print ("o resultado da multiplicação é: ",numero_um_convertido*numero_dois_convertido)
+    print (f"{Fore.LIGHTRED_EX}a operação escolhida foi: multiplição")
+    print (f"{Fore.LIGHTRED_EX}o resultado da multiplicação é: {numero_um_convertido*numero_dois_convertido}")
 elif operacao == "-":
-    print ("a operação escolhida foi: subtração")
-    print ("o resultado da subtração é:",numero_um_convertido-numero_dois_convertido)
+    print (f"{Fore.LIGHTRED_EX}a operação escolhida foi: subtração")
+    print (f"{Fore.LIGHTRED_EX}o resultado da subtração é: {numero_um_convertido-numero_dois_convertido}")
 elif operacao == "/":
     if numero_dois_convertido == 0:
-        print ("Erro: Não é possível dividir por 0.")
+        print (f"{Fore.LIGHTRED_EX}Erro: Não é possível dividir por 0.")
         sys.exit()
-    print ("a operação escolhida foi: divisão")
-    print ("o resultado da divisão é:", numero_um_convertido/numero_dois_convertido)
+    print (f"{Fore.LIGHTRED_EX}a operação escolhida foi: divisão")
+    print (f"{Fore.LIGHTRED_EX}o resultado da divisão é: {numero_um_convertido/numero_dois_convertido}")
 else: 
-    print ("operação inválida, digitar -, +, * ou /")
+    print (f"{Fore.LIGHTRED_EX}operação inválida, digitar -, +, * ou /")
 
-print ("\nobrigada por utilizar nossos serviços :)\n")
+print (f"{Fore.LIGHTRED_EX}\nobrigada por utilizar nossos serviços :)\n")
